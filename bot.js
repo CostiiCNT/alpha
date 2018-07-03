4aaac7572c385c99e5d93dd2c67fa9ebb9964bd8
CostiiCNT <40470857+CostiiCNT@users.noreply.github.com>
2018-07-03 19:02:19 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,11 +9,11 @@ client.on('ready', () => {
 client.login('NDYzNzMzNTgyOTU1OTM3Nzky.Dh0tOg.hJYuJjeWQZbs8LbiaKRTYiMfMXY');
 
 client.on('guildMemberAdd', member => {
-    member.guild.channels.get('463695924158398464').setName(`» Member Count: ${member.guild.memberCount}`);
+    member.guild.channels.get('463736043707826176').setName(`» Member Count: ${member.guild.memberCount}`);
 });
 
 client.on('guildMemberRemove', member => {
-    member.guild.channels.get('463695924158398464').setName(`» Member Count: ${member.guild.memberCount}`);
+    member.guild.channels.get('463736043707826176').setName(`» Member Count: ${member.guild.memberCount}`);
 });
 
 client.on('guildMemberAdd', member => {
